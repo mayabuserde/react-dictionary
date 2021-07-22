9e2e58cd6633d51d29ede25bea398fcedac12ba2
Maya Buser De <maya.buser@gmail.com>
2021-07-22 09:21:32 -0400

    adding axios and api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 export default function Dictionary() {
 
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/sunset`
     let [keyword, setKeyword] = useState('');
+    
+    function handleResponse(response) {
+        console.log(response);
+    }
+    
     function search(event) {
         event.preventDefault();
         alert(`Searching for ${keyword}`);
+        axios.get(apiUrl).then(handleResponse);
 
     }
     
