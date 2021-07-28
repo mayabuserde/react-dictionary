1152ea74c31d66782af3ecf783a54ea1aa20e748
Maya Buser De <maya.buser@gmail.com>
2021-07-28 15:47:31 -0400

    adding phonetics and audio

---
+++ src/Phonetics.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Phonetics(props) {
+    return (
+        <div className="Phonetics" >
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer" >Listen</a>
+        <br/>
+        {props.phonetic.text}</div>
+    )
+}
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
    if (props.results) { 
        return (
            <div className="Results">
            <h2>{props.results.word}</h2>
+           {props.results.phonetics.map((phonetic, index) => {
+            return (
+                <div key={index}>
+                 <Phonetics phonetic={phonetic} />
+                </div>
+
+            )
+           })}
            {props.results.meanings.map((meaning, index) => {
                return (
                    <div key={index}>
